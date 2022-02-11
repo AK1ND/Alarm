4a35014cf68331a634844de3a43e40d820d88dd3
PaulHarokh <pashagorokh@gmail.com>
2022-02-11 19:39:52 +0300

    Made Simple Alarm

---
+++ app/src/main/java/com/alex_kind/alarm/AlarmReceiver.kt
@@ -0,0 +1,32 @@
+package com.alex_kind.alarm
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, p1: Intent?) {
+
+        val intent = Intent(context, SoundActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(context!!, "alarm id")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("My Alarm Manager")
+            .setContentText("Running Alarm")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(123, builder.build())
+
+
+    }
+}
---
+++ app/src/main/java/com/alex_kind/alarm/SoundActivity.kt
@@ -0,0 +1,11 @@
+package com.alex_kind.alarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound)
+    }
+}
