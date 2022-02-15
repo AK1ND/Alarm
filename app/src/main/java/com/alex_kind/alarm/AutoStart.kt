5af4702ad2633481c4c8d51c323c2924b6acea01
Alexander Kishkar <a.kishkar99@gmail.com>
2022-02-15 16:04:06 +0300

    autostart recreate pendings

--- app/src/main/java/com/alex_kind/alarm/AutoStart.kt
+++ app/src/main/java/com/alex_kind/alarm/AutoStart.kt
@@ -1,5 +1,7 @@
 package com.alex_kind.alarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,16 +13,18 @@ import kotlinx.coroutines.launch
 class AutoStart : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        CoroutineScope(Dispatchers.Main).launch {
-            var intent = Intent(context, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context?.startActivity(intent)
 
-            //roll up app when device was rebooted
-//            delay(50)
-//            intent = Intent(Intent.ACTION_MAIN)
-//            intent.addCategory(Intent.CATEGORY_HOME)
-//            context?.startActivity(intent)
+        if (intent?.action.equals("android.intent.action.BOOT_COMPLETED")){
+            val alarmIntent = Intent(context, AlarmReceiver::class.java)
+            val pendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                context, 0, alarmIntent, 0
+            )
+            val manager: AlarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val interval = 8000
+            manager.setInexactRepeating(
+                AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+                interval.toLong(), pendingIntent
+            )
         }
     }
 }
